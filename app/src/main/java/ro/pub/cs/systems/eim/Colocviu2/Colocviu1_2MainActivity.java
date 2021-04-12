73fe16e31fd2bbbbe1f96fdca38ae46621ac098f
mihai_eugen.papa <mihai_eugen.papa@stud.acs.upb.ro>
2021-04-12 10:39:57 +0300

    pana la B.1.

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu2/Colocviu1_2MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu2/Colocviu1_2MainActivity.java
@@ -3,12 +3,37 @@ package ro.pub.cs.systems.eim.Colocviu2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Colocviu1_2MainActivity extends AppCompatActivity {
+public class Colocviu1_2MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_2_main);
+
+        Button add = (Button)findViewById(R.id.add);
+        add.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.add:
+                String x = ((TextView)findViewById(R.id.allTerms)).getText().toString();
+                if (x == "") {
+                    x = x.concat(((EditText)findViewById(R.id.next_term)).getText().toString());
+                    ((TextView)findViewById(R.id.allTerms)).setText(x);
+                    ((EditText)findViewById(R.id.next_term)).setText("");
+                } else {
+                    x = x.concat(" + ");
+                    x = x.concat(((EditText)findViewById(R.id.next_term)).getText().toString());
+                    ((TextView)findViewById(R.id.allTerms)).setText(x);
+                    ((EditText)findViewById(R.id.next_term)).setText("");
+                }
+        }
     }
 }
